ae2edfab1254ee350d37ad8b1c96b6f439c08428
LT. Sana <sana.mngadi@gmail.com>
2022-01-18 23:15:44 +0200

    Update asgi.py

--- DaddysMovie/asgi.py
+++ DaddysMovie/asgi.py
@@ -6,6 +6,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
