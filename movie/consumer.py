ac47b59fbc164c5fc4ff547b39ae8e8c7ceec208
LT. Sana <sana.mngadi@gmail.com>
2022-03-01 00:10:43 +0200

    Update consumer.py

--- movie/consumer.py
+++ movie/consumer.py
@@ -287,6 +287,8 @@ class ChatConsumer(WebsocketConsumer):
 
             print(self.scope["user"])
 
+            self.accept()
+
             # Join room group
             async_to_sync(self.channel_layer.group_add)(
                 self.room_group_name,
