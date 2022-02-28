30c4ea2fedae45a34bd994fa811a5edee82c96ba
LT. Sana <sana.mngadi@gmail.com>
2022-03-01 01:34:47 +0200

    Update settings.py

--- DaddysMovie/settings.py
+++ DaddysMovie/settings.py
@@ -145,7 +145,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS__URL', 'localhost:6379')],
+            "hosts": [os.environ.get('REDIS__URL', 'redis://localhost:6379')],
         },
         "channel_capacity": {
             "http.request": 200,
