8bd718828a69c99b6890119be575ed22147e21bb
LT. Sana <sana.mngadi@gmail.com>
2022-02-28 23:15:37 +0200

    Updates

--- movie/routing.py
+++ movie/routing.py
@@ -4,5 +4,5 @@ from .consumer import ChatConsumer, MovieStatusConsumer
 
 websocket_urlpatterns = [
     path("ws/chat/<slug:sessionID>/", ChatConsumer.as_asgi()),
-    path("ws/movie/<slug:sessionID>/", MovieStatusConsumer.as_asgi()),
+    path("ws/movie/<slug:sessionID>", MovieStatusConsumer.as_asgi()),
 ]
